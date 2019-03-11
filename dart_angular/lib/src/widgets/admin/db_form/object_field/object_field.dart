b563064fd72caaa7fc01657f8ac9c5cba1ec30c6
Navid <navidshad72@gmail.com>
2019-03-11 23:26:53 +0330

    added object field component improved dbField system

---
+++ dart_angular/lib/src/widgets/admin/db_form/object_field/object_field.dart
@@ -0,0 +1,54 @@
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import 'dart:html';
+import 'dart:convert';
+
+import '../../../../class/utility/collection_options.dart';
+
+import '../../db_form/select_item/select_item.dart';
+
+@Component(
+	selector:'object-field',
+	templateUrl: 'object_field.html',
+	styleUrls: ['object_field.scss.css'],
+	directives: [
+		coreDirectives,
+		formDirectives,
+		ObjectField,
+		SelectItem,
+	],
+	exports: [
+		FieldType,
+	]
+)
+class ObjectField 
+{
+	List<DbField> fields = [];
+	bool viewMode = false;
+	
+
+	void addValueToObjectFiled(String key, String value) =>
+		object[key] = value;
+
+	String getValueOfObjectField(key)
+	{
+		String value = '';
+
+		try{
+			value = object[key];
+		}
+		catch(e){}
+
+		return value;
+	}
+
+	@Input('viewMode')
+	void set setViewMode(bool value) => viewMode = value;
+
+	@Input()
+	dynamic object;
+
+	@Input()
+	void set options(List<DbField> value) => fields = value;
+}
